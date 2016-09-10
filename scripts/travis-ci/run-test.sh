aaafaadf6e7bea36d1c2af00624b28bcf3b3dfa3
Pieter Frenssen <pieter@frenssen.be>
2016-09-10 14:49:28 +0200

    Travis Tweakage.

---
+++ scripts/travis-ci/run-test.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+case "$1" in
+    PHP_CodeSniffer)
+        cd $MODULE_DIR
+        composer install
+        ./vendor/bin/phpcs
+        exit $?
+        ;;
+    *)
+        ln -s $MODULE_DIR $DRUPAL_DIR/modules/og
+        cd $DRUPAL_DIR
+        ./vendor/bin/phpunit -c ./core/phpunit.xml.dist $MODULE_DIR
+        exit $?
+esac
